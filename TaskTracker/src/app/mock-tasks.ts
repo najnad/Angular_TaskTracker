1d57b027035e66a41f9d72d7a8439ba1639b93bd
Daniel Chu <n4jn4d@gmail.com>
2021-08-23 16:57:53 -0700

    created task interface and mock tasks

---
+++ TaskTracker/src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+ import {Task} from './Task'
+ 
+ export const TASKS: Task[] = [
+   {
+     id: 1,
+     text: "Finish Assignment 2",
+     day: "August 25, 3:30pm",
+     reminder: true,
+   },
+   {
+    id: 2,
+    text: "Fix Bug",
+    day: "August 29, 11:30pm",
+    reminder: true,
+    },
+    {
+      id: 3,
+      text: "Laundry",
+      day: "August 27, 10:00am",
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: "Doctor's Appt",
+      day: "August 30, 12 :30pm",
+      reminder: true,
+    }
+ ]
